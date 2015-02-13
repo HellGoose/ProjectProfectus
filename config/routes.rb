9d8f31f60afa03372ad1ab0eb3a860216bd8915d
Stian Torjussen <stiant92@gmail.com>
2015-02-13 14:27:12 +0100

    Standarized the Project MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'projects/categories'
+  get 'projects/', to: 'projects#index'
 
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
