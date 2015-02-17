5e465add57a271a89390219b5589feac325b1bcd
Stian Torjussen <stiant92@gmail.com>
2015-02-17 15:04:36 +0100

    Added Default values and Requiered field to Project.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -70,6 +70,6 @@ class ProjectsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
       #params[:project]
-      params.require(:project).permit( :title, :content, :tags, :logoLink)
+      params.require(:project).permit( :title, :description, :content, :tags, :logoLink)
     end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,12 @@
 class Project < ActiveRecord::Base
+	validates :title, :description, :content, presence: true
+	after_initialize :init
+
+
+	def init
+		self.flagged ||= false
+		self.voteCount ||= 0
+		self.finalVoteCount ||= 0
+		self.isGettingFunded ||= false
+	end
 end
