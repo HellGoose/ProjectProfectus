8f2d80dc186f59c6440951dd146db9fb7d27f841
Stian Torjussen <stiant92@gmail.com>
2015-02-17 15:46:56 +0100

    ProjectModel and Controller HotFix

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,17 @@
 class Project < ActiveRecord::Base
+	#Required fields
 	validates :title, :description, :content, presence: true
+
+	#Relations
+	has_many :iterations
+
+	#Sets default values
 	after_initialize :init
-
-
 	def init
-		self.flagged ||= false
-		self.voteCount ||= 0
-		self.finalVoteCount ||= 0
-		self.isGettingFunded ||= false
+		print(self)
+		self.flagged = false if (self.has_attribute? :flagged) && self.flagged.nil?
+		self.voteCount ||= 0 if self.has_attribute? :voteCount
+		self.finalVoteCount ||= 0 if self.has_attribute? :finalVoteCount
+		self.isGettingFunded = false if (self.has_attribute? :isGettingFunded) && self.isGettingFunded.nil?
 	end
 end
