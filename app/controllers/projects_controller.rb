ac44dbd0a6183de2d8ea07cc9a9e593dfacd7bc5
Stian Torjussen <stiant92@gmail.com>
2015-02-20 10:46:10 +0100

    Added Delete/Edit Restrictions to Project.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -57,7 +57,7 @@ class ProjectsController < ApplicationController
   # PATCH/PUT /projects/1.json
   def update
     respond_to do |format|
-      if @project.update(project_params)
+      if @project.user_id == session[:user_id] && @project.update(project_params)
         format.html { redirect_to @project, notice: 'Project was successfully updated.' }
         format.json { render :show, status: :ok, location: @project }
       else
