9108ddfce56b3907c5a7dc5f203ee1d3143f48a3
kristh <kris.th@gmail.com>
2015-02-23 16:04:58 +0100

    Fixed user destroy controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
 
@@ -23,6 +23,14 @@ class UsersController < ApplicationController
         format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to "/signout", notice: 'User was deleted.' }
+      format.json { head :no_content }
     end
   end
 
