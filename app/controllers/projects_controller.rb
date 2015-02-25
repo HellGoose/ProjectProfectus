dd1adc5aefef5c6e6af81f54c3d8aa5f448c748f
Stian Torjussen <stiant92@gmail.com>
2015-02-25 12:16:12 +0100

    Added category to project form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -100,7 +100,7 @@ class ProjectsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
       params.require(:project).permit( :title, :description, :content, :tags, :logoLink,
-                    :voteCount, :finalVoteCount, :flagged, :isGettingFunded)
+                    :voteCount, :finalVoteCount, :flagged, :isGettingFunded, :category_id)
     end
 
     def isProjectOwner
