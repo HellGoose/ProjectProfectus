2746841e915e12962fbd628606f686cf9eb9da9f
Stian Torjussen <stiant92@gmail.com>
2015-04-08 15:34:26 +0200

    Add Go-To-Site-Button

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -99,7 +99,7 @@ class CampaignsController < ApplicationController
       		userVote.save
       		@campaign.save
       		respond_to do |format|
-        		msg = { :status => "ok", :message => @campaign.voteCount }
+        		msg = { :status => 'ok', :message => @campaign.voteCount }
         		format.json  { render :json => msg }
       		end
     	end
