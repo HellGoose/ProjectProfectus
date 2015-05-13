bf8975103429b89922993b65658cca22b0bf7486
Runar Heggset <r-heggse@online.no>
2015-05-13 12:37:58 +0200

    Init database script + bugfixes + coffee comments

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,4 +1,16 @@
 class Round < ActiveRecord::Base
 	has_many :winnerUsers, class_name: "RoundWinnerUser"
 	has_many :winnerCampaigns, class_name: "RoundWinnerCampaign"
+
+	after_initialize :init
+
+	private
+		#Sets default values
+		def init
+			self.duration ||= 3600 if self.has_attribute? :duration
+			self.forceNewRound ||= false if self.has_attribute? :forceNewRound
+			self.decayRate ||= 0.75 if self.has_attribute? :decayRate
+			self.currentRound ||= 0 if self.has_attribute? :currentRound
+			self.endTime ||= Time.at(Time.now.to_i + self.duration).to_datetime if self.has_attribute? :endTime
+		end
 end
