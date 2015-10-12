7a0a64e62217affd5768cacadc97f529e34ff462
Runar Heggset <r-heggse@online.no>
2015-10-12 10:13:02 +0200

    Android app now working properly with session cookies

--- mobile/app/src/main/java/io/podium/LoginActivity.java
+++ mobile/app/src/main/java/io/podium/LoginActivity.java
@@ -1,10 +1,16 @@
 package io.podium;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.CookieSyncManager;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String SESSION_COOKIE = "_profectus_session";
+    private static String SESSION;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +20,17 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void loadWebPage() {
-        WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("http://stianerkul.me/mobile/login");
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl("http://www.thepodium.io/mobile/login");
+        webView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                //Users will be notified in case there's an error (i.e. no internet connection)
+                //Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                CookieSyncManager.getInstance().sync();
+            }
+        });
     }
 }
