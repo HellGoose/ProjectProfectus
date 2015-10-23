1d6090c94549e583d825788b6b51f9bad08b463f
Runar Heggset <r-heggse@online.no>
2015-10-23 02:09:14 +0200

    Removed jquery.turbolinks, as it broke existing functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
@@ -110,3 +109,4 @@ var ready = function() {
 };
 
 $(document).ready(ready);
+$(document).on('page:load', ready);
